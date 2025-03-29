9ef951473a10395081349743d7df5e395a6c8343
ajabbar17 <ajabbar1234@gmail.com>
2025-03-29 19:12:54 +0500

    localhost testing

--- client/src/lib/firebase.ts
+++ client/src/lib/firebase.ts
@@ -4,22 +4,14 @@ dotenv.config();
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// const firebaseConfig = {
-//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-//   authDomain: `${import.meta.env.VITE_FIREBASE_PROJECT_ID}.firebaseapp.com`,
-//   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-//   storageBucket: `${import.meta.env.VITE_FIREBASE_PROJECT_ID}.appspot.com`,
-//   appId: import.meta.env.VITE_FIREBASE_APP_ID,
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyC98dv8pkoBsHcNBLNtuNjtuup7c-GDHso",
-  authDomain: "aiml-9fd3c.firebaseapp.com",
-  projectId: "aiml-9fd3c",
-  storageBucket: "aiml-9fd3c.firebasestorage.app",
-  messagingSenderId: "579784540286",
-  appId: "1:579784540286:web:6527364372cccac0bb982f",
-  measurementId: "G-6BH7CSB5EC"
+  apiKey: process.env.VITE_FIREBASE_API_KEY,
+  authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN || `${process.env.VITE_FIREBASE_PROJECT_ID}.firebaseapp.com`,
+  projectId: process.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET || `${process.env.VITE_FIREBASE_PROJECT_ID}.appspot.com`,
+  messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VITE_FIREBASE_APP_ID,
+  measurementId: process.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
